2140c4273c27b8cf6d74bee9da495c4740e763df
Rafaela Dias <rafaeladiasneves95@gmail.com>
2023-02-08 01:26:54 -0300

    Section Games finalizada

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,17 +1,38 @@
 var slide_thumbnail = new Swiper(".slide-thumbnail", {
   slidesPerView: 5,
-  direction: 'vertical',
+  direction: "vertical",
   spaceBetween: 20,
-  watchSlidesProgress: true
+  watchSlidesProgress: true,
 });
 
 var slide_hero = new Swiper(".slide-principal", {
-    effect: 'fade',
-    thumbs: {
-      swiper: slide_thumbnail,
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    }
+  effect: "fade",
+  thumbs: {
+    swiper: slide_thumbnail,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+const allFilters = document.querySelectorAll(".js-nav-games li a");
+const tabPane = document.querySelectorAll(".tab-pane-games");
+
+allFilters.forEach((filter, index) => {
+  filter.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    allFilters.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    tabPane.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    tabPane[index].classList.add("active");
+
+    filter.classList.add("active");
   });
+});
